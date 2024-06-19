465d5e0eadef84f9ac6b1f94e94e3aeb84524516
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-19 16:22:18 -0500

    feat(project): :sparkles: add sendEmail function

--- service.js
+++ service.js
@@ -1,1 +1,34 @@
-console.log("service.js loaded");
+import brevo from "@getbrevo/brevo";
+
+const apiInstance = new brevo.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
+
+async function sendEmail() {
+
+    try {
+        const sendSmtpEmail = new brevo.SendSmtpEmail();
+
+        sendSmtpEmail.subject = "Hello, World!";
+        sendSmtpEmail.to = [
+          { email: "infinitygoat.en@gmail.com", name: "Infinity xD" },
+        ];
+        sendSmtpEmail.htmlContent =
+          "<html><body><h1>Hello, world!</h1><p>This is a test email</p><a href='https://github.com/SkivaDev'>Go to my repository</a></body></html>";
+      
+        sendSmtpEmail.sender = {
+          name: "SkivaDev",
+          email: "fabrizioortizo.main@gmail.com",
+        };
+      
+        const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+sendEmail();
